bf5a03a4072b115e703a8d72f6c94dc561d83ba0
Arjit Mittal <am791@snu.edu.in>
2025-01-05 12:43:39 +0530

    Added World Time zones

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ let hr = document.getElementById('hour');
 let min = document.getElementById('min');
 let sec = document.getElementById('sec');
 const realTimedata = document.querySelector('[real-time-data]');
+const timeZones = moment.tz.names();
+
+const selectElemnt = document.getElementById('time-zone-dropdown');
+timeZones.forEach((zone) => {
+    const option = document.createElement('option');
+    option.value = zone;
+    option.textContent = zone;
+    selectElemnt.appendChild(option);
+});
 
 window.onload = function() {
     setTimeout(function() {
@@ -18,18 +27,22 @@ window.onload = function() {
 
 
 function displayTime() {
-    let date = new Date();
-    let hh = date.getHours();
-    let mm = date.getMinutes();
-    let ss = date.getSeconds();
+    const selectedZone = document.getElementById('time-zone-dropdown').value;
+    if(selectedZone) {
+        const localTime = moment().tz(selectedZone);
+        let hh = localTime.format('HH');
+        let mm = localTime.format('mm');
+        let ss = localTime.format('ss');
 
-    let hRotation = 30*hh + mm/2;
-    let mRotation = 6*mm;
-    let sRotation = 6*ss;
+        let hRotation = 30*hh + mm/2;
+        let mRotation = 6*mm;
+        let sRotation = 6*ss;
 
-    hr.style.transform = `rotate(${hRotation}deg)`;
-    min.style.transform = `rotate(${mRotation}deg)`;
-    sec.style.transform = `rotate(${sRotation}deg)`;
+        hr.style.transform = `rotate(${hRotation}deg)`;
+        min.style.transform = `rotate(${mRotation}deg)`;
+        sec.style.transform = `rotate(${sRotation}deg)`;
 
-    realTimedata.innerText = `Time: ${String(hh).padStart(2, '0')}:${String(mm).padStart(2, '0')}:${String(ss).padStart(2, '0')}`;
+        realTimedata.innerText = `Time: ${String(hh).padStart(2, '0')}:${String(mm).padStart(2, '0')}:${String(ss).padStart(2, '0')}`;
+    }
+    
 }
